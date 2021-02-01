c60a8eac7aad71276572de86c04e4f01eb5f6e7b
robb <roberto.manganelly@gmail.com>
2021-02-01 01:27:12 -0500

    refactor Sales_form to sales_tool_dialog.py

--- components/menus.py
+++ components/menus.py
@@ -4,7 +4,8 @@ from components.data_display_tab import execute_display_table
 from dialogs.auxiliar_dialogs import selfCloseInterface
 from dialogs.capital_tool_dialog import CapitalFormDialog
 from dialogs.purchases_tool_dialog import PurchasesFormDialog
-from dialogs.tool_dialogs import tool_launcher, SalesFormDialog, HelpOfflineDialog
+from dialogs.sales_tool_dialog import SalesFormDialog
+from dialogs.tool_dialogs import tool_launcher, HelpOfflineDialog
 from modules.data_import_export import import_data_to_diary, export_data_from_diary
 from modules.db_templates_manager import connect_toDB
 from routines.change_session_date import manage_date_session
